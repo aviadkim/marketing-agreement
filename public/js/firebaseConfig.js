2c784c381f6d84e9036b4991630018b189672382
aviadkim <aviad@kimfo-fs.com>
2024-12-13 11:05:55 +0200

    Update firebaseConfig.js

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -1,1 +1,18 @@
+// public/js/firebaseConfig.js
+const firebaseConfig = {
+    apiKey: "AIzaSyBlrfwQJmkUSnqoNZp3bxfH9DH0QuuJtMs",
+    authDomain: "client-d5bfe.firebaseapp.com",
+    projectId: "client-d5bfe",
+    storageBucket: "client-d5bfe.appspot.com",
+    messagingSenderId: "678297464867",
+    appId: "1:678297464867:web:2c929a45d2e9f0cdb68196"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+// Enable debugging
+firebase.firestore.setLogLevel('debug');
+firebase.storage().setLogLevel('debug');
